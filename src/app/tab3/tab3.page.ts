19ec46b9b26dfe85b8434abb58a992b56d7c0d90
Jairo C. Assis <jairochvs@hotmail.com>
2020-10-03 02:12:59 -0400

    Added some notes to tab 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,22 +8,26 @@ import { Collections } from '../collections'
 })
 export class Tab3Page {
 
-  gamesList =  Collections.gamesList;
+  // Contains the collection of my favorite games.
+  gamesList = Collections.gamesList;
+  // Flag that determines whether a new game is currently being added to the list.
   isAddingNewGame = false;
+  // The new game value that should be added to the list.
   newValue = "";
 
-  constructor() {}
+  constructor() { }
 
-  onEnableAdd(){
+  // Enables the Add New Game functionality.
+  onEnableAdd() {
     this.isAddingNewGame = true;
   }
 
-  onAddNew(){
+  // Click event for the done button that adds the new value to the collection.
+  onAddNew() {
     this.isAddingNewGame = false;
-    if(this.newValue !== null && this.newValue !== '') {
+    if (this.newValue !== null && this.newValue !== '') {
       this.gamesList.push(this.newValue);
     }
     this.newValue = "";
   }
-
 }
