29e6d1da7a192a414ae4e2f36d1b83daa1bc8cf9
Jairo C. Assis <jairochvs@hotmail.com>
2020-10-03 03:43:42 -0400

    Modified Tab 3 to hold the cover page of the game.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Collections } from '../collections'
+import { Collections, GameInfo } from '../collections'
 
 @Component({
   selector: 'app-tab3',
@@ -9,11 +9,12 @@ import { Collections } from '../collections'
 export class Tab3Page {
 
    // Contains the collection of my favorite games.
-   itemList = Collections.gamesList;
+   itemList = Collections.rankedGameList;
    // Flag that determines whether a new game is currently being added to the list.
    isAddingNewItem = false;
    // The new game value that should be added to the list.
-   newValue = "";
+   newUrl = null;
+   newName = null;
  
    constructor() { }
  
@@ -25,9 +26,16 @@ export class Tab3Page {
    // Click event for the done button that adds the new value to the collection.
    onAddNew() {
      this.isAddingNewItem = false;
-     if (this.newValue !== null && this.newValue !== '') {
-       this.itemList.push(this.newValue);
+     if (this.newName !== null && this.newName !== '' && this.newUrl !== null && this.newUrl !== '') {
+       // Create a new GameInfo object.
+       var newGame = new GameInfo();
+       newGame.GameTitle = this.newName;
+       newGame.Url = this.newUrl;
+         
+       
+       this.itemList.push(newGame);
      }
-     this.newValue = "";
+     this.newName = "";
+     this.newUrl = "";
    }
 }
